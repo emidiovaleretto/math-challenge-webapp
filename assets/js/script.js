e5a7e127e8758e47876e4aa42f3bb9c6a1abdc18
Emidio Valereto <emidio.valereto@gmail.com>
2021-10-25 19:06:51 +0100

    Fix code in the subtraction game.

--- assets/js/script.js
+++ assets/js/script.js
@@ -129,8 +129,8 @@ function displayAdditionQuestion(operand1, operand2) {
 
 function displaySubtractQuestion(operand1, operand2) {
 
-    document.getElementById('operand1').textContent = operand1;
-    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById('operand2').textContent = operand1 > operand2 ? operand2 : operand1;
     document.getElementById('operator').textContent = "-";
 
 }
